5f999c79e3b1c57db43c912d2cd9dda15465e04f
LegendaryCodeSeeker <seeker.of.funz@gmail.com>
2020-04-09 17:28:36 -0500

    wip death sign

--- src/main/java/j0sh/javadungeons/blocks/DungeonsDeathSign.java
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsDeathSign.java
@@ -1,7 +1,18 @@
 package j0sh.javadungeons.blocks;
 
-import net.minecraft.item.Items;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.SignBlock;
+import net.minecraft.util.SignType;
 
-public class DungeonsDeathSign {
+public class DungeonsDeathSign extends SignBlock {
 
+    public BlockItem blockItem;
+    
+    public DungeonsDeathSign(Settings settings, SignType signType) {
+        super(settings, signType);
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    
 }
