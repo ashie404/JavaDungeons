bd94d27f55a922dc1034ab98807611d6935007f3
thejshua <joyesh708@gmail.com>
2020-04-09 10:42:54 -0500

    add rocky dirt

--- src/main/java/j0sh/javadungeons/content/GenericBlocks.java
+++ src/main/java/j0sh/javadungeons/content/GenericBlocks.java
@@ -11,6 +11,7 @@ public class GenericBlocks {
     public static DungeonsBlock DIRTY_FLOOR_TILE; 
     public static DungeonsBlock CRATE;
     public static DungeonsBlock DIRT;
+    public static DungeonsBlock ROCKY_DIRT;
     public static DungeonsBlock ASH_PLANKS;
 
     public static void init() {
@@ -18,6 +19,7 @@ public class GenericBlocks {
         DIRTY_FLOOR_TILE = new DungeonsBlock(Blocks.ANDESITE, JavaDungeons.GENERIC, "dirty_floor_tile");
         CRATE = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "crate");
         DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "dirt");
+        ROCKY_DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "rocky_dirt");
         ASH_PLANKS = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "ash_planks");
     }
 }
