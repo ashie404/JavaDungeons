2300637e9fe084ce04a15b53cf9d73a3d27cb86e
thejshua <joyesh708@gmail.com>
2020-04-09 08:17:15 -0500

    add dirt (adding different types later)

--- src/main/java/j0sh/javadungeons/content/GenericBlocks.java
+++ src/main/java/j0sh/javadungeons/content/GenericBlocks.java
@@ -9,9 +9,11 @@ public class GenericBlocks {
 
     public static DungeonsBlock FLOOR_TILE;
     public static DungeonsBlock CRATE;
+    public static DungeonsBlock DIRT;
 
     public static void init() {
         FLOOR_TILE = new DungeonsBlock(Blocks.ANDESITE, JavaDungeons.GENERIC, "floor_tile");
         CRATE = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "crate");
+        DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "dirt");
     }
 }
