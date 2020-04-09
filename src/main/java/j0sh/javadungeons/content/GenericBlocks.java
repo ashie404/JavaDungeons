40e0eeaa41b6b48dcbd75f46d9cd75e522051679
thejshua <joyesh708@gmail.com>
2020-04-09 10:52:36 -0500

    add dirt slab and lang for some blocks

--- src/main/java/j0sh/javadungeons/content/GenericBlocks.java
+++ src/main/java/j0sh/javadungeons/content/GenericBlocks.java
@@ -11,6 +11,7 @@ public class GenericBlocks {
     public static DungeonsBlock DIRTY_FLOOR_TILE; 
     public static DungeonsBlock CRATE;
     public static DungeonsBlock DIRT;
+    public static DungeonsSlab DIRT_SLAB;
     public static DungeonsBlock ROCKY_DIRT;
     public static DungeonsBlock ASH_PLANKS;
 
@@ -19,6 +20,7 @@ public class GenericBlocks {
         DIRTY_FLOOR_TILE = new DungeonsBlock(Blocks.ANDESITE, JavaDungeons.GENERIC, "dirty_floor_tile");
         CRATE = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "crate");
         DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "dirt");
+        DIRT_SLAB = new DungeonsSlab(Blocks.DIRT, JavaDungeons.GENERIC, "dirt_slab");
         ROCKY_DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "rocky_dirt");
         ASH_PLANKS = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "ash_planks");
     }
