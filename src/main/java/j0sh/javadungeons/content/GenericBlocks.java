7c4d9bb6f229c019750f1074871225e171c770be
thejshua <joyesh708@gmail.com>
2020-04-09 19:38:01 -0500

    add dark stone

--- src/main/java/j0sh/javadungeons/content/GenericBlocks.java
+++ src/main/java/j0sh/javadungeons/content/GenericBlocks.java
@@ -7,27 +7,46 @@ import j0sh.javadungeons.JavaDungeons;
 
 public class GenericBlocks {
 
-    // generic blocks
+    // generic blocks that aren't in mc
     public static DungeonsBlock FLOOR_TILE;
     public static DungeonsBlock DIRTY_FLOOR_TILE; 
     public static DungeonsBlock CRATE;
+
+    // ground related
     public static DungeonsBlock DIRT;
     public static DungeonsSlab DIRT_SLAB;
     public static DungeonsBlock ROCKY_DIRT;
+
+    // wood related
     public static DungeonsBlock ASH_PLANKS;
+
+    // stone-brick related
+    public static DungeonsBlock DARK_STONE_BRICKS;
+    public static DungeonsBlock DIRTY_DARK_STONE_BRICKS;
+    public static DungeonsBlock CHISELED_DARK_STONE;
 
     // generic plants
     public static DungeonsPlant SHORT_GRASS;
     public static DungeonsPlant GRASS_CLUMP;
 
     public static void init() {
+        // dungeons blocks that aren't in mc
         FLOOR_TILE = new DungeonsBlock(Blocks.ANDESITE, JavaDungeons.GENERIC, "floor_tile");
         DIRTY_FLOOR_TILE = new DungeonsBlock(Blocks.ANDESITE, JavaDungeons.GENERIC, "dirty_floor_tile");
         CRATE = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "crate");
+
+        // ground related
         DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "dirt");
         DIRT_SLAB = new DungeonsSlab(Blocks.DIRT, JavaDungeons.GENERIC, "dirt_slab");
         ROCKY_DIRT = new DungeonsBlock(Blocks.DIRT, JavaDungeons.GENERIC, "rocky_dirt");
+
+        // wood related
         ASH_PLANKS = new DungeonsBlock(Blocks.OAK_PLANKS, JavaDungeons.GENERIC, "ash_planks");
+
+        // stone brick related
+        DARK_STONE_BRICKS = new DungeonsBlock(Blocks.STONE_BRICKS, JavaDungeons.GENERIC, "dark_stone_bricks");
+        DIRTY_DARK_STONE_BRICKS = new DungeonsBlock(Blocks.STONE_BRICKS, JavaDungeons.GENERIC, "dirty_dark_stone_bricks");
+        CHISELED_DARK_STONE = new DungeonsBlock(Blocks.CHISELED_STONE_BRICKS, JavaDungeons.GENERIC, "chiseled_dark_stone");
 
         // plants
         SHORT_GRASS = new DungeonsPlant(Blocks.GRASS, JavaDungeons.GENERIC, "short_grass");
