46ee6f4233445eed62c682b49a276eb16dafbaac
thejshua <joyesh708@gmail.com>
2020-04-10 16:50:38 -0500

    variant was misspelled

--- src/main/java/j0sh/javadungeons/JavaDungeons.java
+++ src/main/java/j0sh/javadungeons/JavaDungeons.java
@@ -21,7 +21,7 @@ public class JavaDungeons implements ModInitializer {
 	public static final ItemGroup GENERIC = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "generic"), () -> new ItemStack(GenericBlocks.FLOOR_TILE));
 	public static final ItemGroup CREEPER_WOODS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "creeper_woods"), () -> new ItemStack(CreeperWoodsBlocks.CW_MOSSY_STONE_BRICKS));
 	public static final ItemGroup DESERT_TEMPLE = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "desert_temple"), () -> new ItemStack(DesertTempleBlocks.DT_CHISELED_SANDY_ANDESITE));
-	public static final ItemGroup CUSTOM_VARIENTS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "misc"), () -> new ItemStack(GenericBlocks.DIMENSIONAL_RECTIFIER));
+	public static final ItemGroup CUSTOM_VARIANTS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "misc"), () -> new ItemStack(GenericBlocks.DIMENSIONAL_RECTIFIER));
 
 	@Override
 	public void onInitialize() {
