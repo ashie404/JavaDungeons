42827a7d24d312a1d14dae1cfdb9354d92f20aec
thejshua <joyesh708@gmail.com>
2020-05-16 15:47:24 -0500

    fix lantern not glowing and plant bounding boxes

--- src/main/java/j0sh/javadungeons/blocks/DungeonsHangingPlant.java
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsHangingPlant.java
@@ -3,6 +3,7 @@ package j0sh.javadungeons.blocks;
 import j0sh.javadungeons.JavaDungeons;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -13,6 +14,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.WorldView;
 import net.minecraft.sound.BlockSoundGroup;
@@ -22,6 +25,12 @@ public class DungeonsHangingPlant extends Block {
     // hanging plant block
 
     public BlockItem blockItem;
+
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(4.0D, -16.0D, 4.0D, 12.0D, 16.0D, 12.0D);
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
+        return SHAPE;
+    }
 
     public DungeonsHangingPlant(Material material, BlockSoundGroup sounds, ItemGroup group, String id) {
         super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().collidable(false));
