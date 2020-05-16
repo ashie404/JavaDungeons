b8c868f3e5ded3bcef6be8da3648af984e3bdad7
thejshua <joyesh708@gmail.com>
2020-05-16 13:57:02 -0500

    add hanging roses

---
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsHangingPlant.java
@@ -0,0 +1,40 @@
+package j0sh.javadungeons.blocks;
+
+import j0sh.javadungeons.JavaDungeons;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.WorldView;
+import net.minecraft.sound.BlockSoundGroup;
+
+public class DungeonsHangingPlant extends Block {
+
+    // hanging plant block
+
+    public BlockItem blockItem;
+
+    public DungeonsHangingPlant(Material material, BlockSoundGroup sounds, ItemGroup group, String id) {
+        super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().collidable(false));
+        Registry.register(Registry.BLOCK, new Identifier(JavaDungeons.MOD_ID, id), this);
+        Registry.register(Registry.ITEM,new Identifier(JavaDungeons.MOD_ID, id), blockItem = new BlockItem(this, new Item.Settings().group(group)));
+    }
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
+        return facing == Direction.UP && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+    }
+  
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return sideCoversSmallSquare(world, pos.up(), Direction.DOWN);
+    }
+
+}
