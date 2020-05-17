f85a77e1bdde7275f3072e4c2537a53cffcd3bf0
thejshua <joyesh708@gmail.com>
2020-05-17 10:08:42 -0500

    add candle

--- src/main/java/j0sh/javadungeons/blocks/DungeonsCandle.java
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsCandle.java
@@ -11,7 +11,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -25,26 +25,23 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.world.IWorld;
 
-public class DungeonsBrazier extends Block {
+public class DungeonsCandle extends Block {
 
-    // brazier block
+    // candle block
 
     public BlockItem blockItem;
 
-    public String type;
-
-    protected static final VoxelShape SHAPE = Block.createCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 13.0D, 15.0D);
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(5.5D, 0.0D, 5.5D, 10.5D, 15.0D, 10.5D);
 
     @Override
-    public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, ShapeContext entityCtx) {
+    public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, EntityContext entityCtx) {
 		return SHAPE;
 	}
 
-    public DungeonsBrazier(Material material, BlockSoundGroup sounds, ItemGroup group, String type, String id) {
-        super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().lightLevel(type != "unlit" ? 15 : 0));
-        this.type = type;
+    public DungeonsCandle(Material material, BlockSoundGroup sounds, ItemGroup group, String id) {
+        super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().lightLevel(15));
         Registry.register(Registry.BLOCK, new Identifier(JavaDungeons.MOD_ID, id), this);
         Registry.register(Registry.ITEM,new Identifier(JavaDungeons.MOD_ID, id), blockItem = new BlockItem(this, new Item.Settings().group(group)));
     }
@@ -55,18 +52,16 @@ public class DungeonsBrazier extends Block {
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
         return facing == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
     }
 
     @Environment(EnvType.CLIENT)
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        if (type != "unlit") {
-            double d = (double)pos.getX() + 0.5D;
-            double e = (double)pos.getY() + 0.7D;
-            double f = (double)pos.getZ() + 0.5D;
-            world.addParticle(ParticleTypes.SMOKE, d, e, f, 0.0D, 0.0D, 0.0D);
-            world.addParticle(type != "green_lit" ? ParticleTypes.FLAME : Particles.GREEN_FLAME, d, e, f, 0.0D, 0.0D, 0.0D);
-        }
+        double d = (double)pos.getX() + 0.5D;
+        double e = (double)pos.getY() + 1.0D;
+        double f = (double)pos.getZ() + 0.5D;
+        world.addParticle(ParticleTypes.SMOKE, d, e, f, 0.0D, 0.0D, 0.0D);
+        world.addParticle(ParticleTypes.FLAME, d, e, f, 0.0D, 0.0D, 0.0D);
     }
 }
