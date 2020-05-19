0e57671267297991b4b31228855528bc4a20ae80
thejshua <joyesh708@gmail.com>
2020-05-19 12:31:13 -0500

    towelette compatibility

--- src/main/java/j0sh/javadungeons/blocks/DungeonsWaterPlant.java
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsWaterPlant.java
@@ -2,20 +2,41 @@ package j0sh.javadungeons.blocks;
 
 import j0sh.javadungeons.JavaDungeons;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidFillable;
 import net.minecraft.block.Material;
-import net.minecraft.block.SeagrassBlock;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.block.PlantBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class DungeonsWaterPlant extends SeagrassBlock {
+public class DungeonsWaterPlant extends PlantBlock implements FluidFillable {
 
     // water plant block
 
     public BlockItem blockItem;
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        return fluidState.matches(FluidTags.WATER) && fluidState.getLevel() == 8 ? super.getPlacementState(ctx) : null;
+    }
+
+    protected boolean canPlantOnTop(BlockState floor, BlockView view, BlockPos pos) {
+        return floor.isSideSolidFullSquare(view, pos, Direction.UP) && floor.getBlock() != Blocks.MAGMA_BLOCK;
+    }
 
     public DungeonsWaterPlant(Material material, BlockSoundGroup sounds, ItemGroup group, String id) {
         super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().collidable(false));
@@ -23,4 +44,15 @@ public class DungeonsWaterPlant extends SeagrassBlock {
         Registry.register(Registry.ITEM,new Identifier(JavaDungeons.MOD_ID, id), blockItem = new BlockItem(this, new Item.Settings().group(group)));
     }
 
+    public boolean canFillWithFluid(BlockView view, BlockPos pos, BlockState state, Fluid fluid) {
+        return false;
+    }
+  
+    public boolean tryFillWithFluid(WorldAccess world, BlockPos pos, BlockState state, FluidState fluidState) {
+        return false;
+    }
+
+    public FluidState getFluidState(BlockState state) {
+        return Fluids.WATER.getStill(false);
+    }
 }
--- src/main/java/j0sh/javadungeons/mixin/CompatMixinWaterPlant.java
+++ src/main/java/j0sh/javadungeons/mixin/CompatMixinWaterPlant.java
@@ -2,20 +2,17 @@ package j0sh.javadungeons.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import virtuoel.towelette.api.Fluidloggable;
 import j0sh.javadungeons.blocks.DungeonsWaterPlant;
 import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
+import virtuoel.towelette.api.Fluidloggable;
+import virtuoel.towelette.util.FluidUtils;
 
 @Mixin(DungeonsWaterPlant.class)
 public class CompatMixinWaterPlant implements Fluidloggable {
     @Overwrite
     public FluidState getFluidState(BlockState state) {
-        return virtuoel.towelette.util.FluidUtils.getFluidState(state);
+        return FluidUtils.getFluidState(state);
     }
 }
