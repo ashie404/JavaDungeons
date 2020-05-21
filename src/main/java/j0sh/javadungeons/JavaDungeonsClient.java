61c0f5e1a6c592a32feb0d9f481fd51eb3839fed
thejshua <joyesh708@gmail.com>
2020-05-21 08:02:43 -0500

    fix serverside crash caused by fluid rendering

--- src/main/java/j0sh/javadungeons/JavaDungeonsClient.java
+++ src/main/java/j0sh/javadungeons/JavaDungeonsClient.java
@@ -3,15 +3,30 @@ package j0sh.javadungeons;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.item.ItemColorProvider;
 import net.minecraft.client.color.world.GrassColors;
 import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockRenderView;
+
+import java.util.function.Function;
 
 import j0sh.javadungeons.content.*;
 import j0sh.javadungeons.fluids.DungeonsWaterFluid;
@@ -129,11 +144,68 @@ public class JavaDungeonsClient implements ClientModInitializer {
             registry.register(new Identifier(JavaDungeons.MOD_ID, "particle/green_flame"));
         });
 
-        DungeonsWaterFluid.setupFluidRendering(
+        setupFluidRendering(
             Fluids.DUNGEONS_WATER_STILL, // still fluid object
             Fluids.DUNGEONS_WATER_FLOWING, // flowing fluid object
             new Identifier(JavaDungeons.MOD_ID, "dungeons_water"), // texture identifier
             0xFFFFFF // tint color (white because water is colored in its file)
         );
     }    
+
+    public static void setupFluidRendering(final Fluid still, final Fluid flowing, final Identifier textureFluidId, final int color)
+	{
+		final Identifier stillSpriteId = new Identifier(textureFluidId.getNamespace(), "block/" + textureFluidId.getPath() + "_still");
+		final Identifier flowingSpriteId = new Identifier(textureFluidId.getNamespace(), "block/" + textureFluidId.getPath() + "_flow");
+ 
+		// If they're not already present, add the sprites to the block atlas
+		ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) ->
+		{
+			registry.register(stillSpriteId);
+			registry.register(flowingSpriteId);
+		});
+ 
+		final Identifier fluidId = Registry.FLUID.getId(still);
+		final Identifier listenerId = new Identifier(fluidId.getNamespace(), fluidId.getPath() + "_reload_listener");
+ 
+		final Sprite[] fluidSprites = { null, null };
+ 
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener()
+		{
+			@Override
+			public Identifier getFabricId()
+			{
+				return listenerId;
+			}
+ 
+			/**
+			 * Get the sprites from the block atlas when resources are reloaded
+			 */
+			@Override
+			public void apply(ResourceManager resourceManager)
+			{
+				final Function<Identifier, Sprite> atlas = MinecraftClient.getInstance().getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEX);
+				fluidSprites[0] = atlas.apply(stillSpriteId);
+				fluidSprites[1] = atlas.apply(flowingSpriteId);
+			}
+		});
+ 
+		// The FluidRenderer gets the sprites and color from a FluidRenderHandler during rendering
+		final FluidRenderHandler renderHandler = new FluidRenderHandler()
+		{
+			@Override
+			public Sprite[] getFluidSprites(BlockRenderView view, BlockPos pos, FluidState state)
+			{
+				return fluidSprites;
+			}
+ 
+			@Override
+			public int getFluidColor(BlockRenderView view, BlockPos pos, FluidState state)
+			{
+				return color;
+			}
+		};
+ 
+		FluidRenderHandlerRegistry.INSTANCE.register(still, renderHandler);
+		FluidRenderHandlerRegistry.INSTANCE.register(flowing, renderHandler);
+	}
 }
