52c6044cff2bd5b447e31e5ee2083f43ca4b7e3f
thejshua <joyesh708@gmail.com>
2020-05-23 12:25:43 -0500

    add all cacti canyon blocks. need to do plants

--- src/main/java/j0sh/javadungeons/JavaDungeons.java
+++ src/main/java/j0sh/javadungeons/JavaDungeons.java
@@ -22,6 +22,7 @@ public class JavaDungeons implements ModInitializer {
 	public static final ItemGroup CREEPER_WOODS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "creeper_woods"), () -> new ItemStack(CreeperWoodsBlocks.CW_MOSSY_STONE_BRICKS));
 	public static final ItemGroup DESERT_TEMPLE = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "desert_temple"), () -> new ItemStack(DesertTempleBlocks.DT_CHISELED_SANDY_STONE));
 	public static final ItemGroup PUMPKIN_PASTURES = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "pumpkin_pastures"), () -> new ItemStack(PumpkinPasturesBlocks.PM_RED_AUTUMNAL_LEAVES));
+	public static final ItemGroup CACTI_CANYON = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "cacti_canyon"), () -> new ItemStack(PumpkinPasturesBlocks.PM_RED_AUTUMNAL_LEAVES));
 	public static final ItemGroup WEAPONS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "weapons"), () -> new ItemStack(Weapons.FIREBRAND));
 
 	@Override
--- src/main/java/j0sh/javadungeons/content/CactiCanyonBlocks.java
+++ src/main/java/j0sh/javadungeons/content/CactiCanyonBlocks.java
@@ -7,9 +7,45 @@ import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class CactiCanyonBlocks {
+
+    // sand related
+    public static DungeonsFallingBlock CC_SAND;
+    public static DungeonsBlock CC_DENSE_SANDY_DIRT;
+    public static DungeonsBlock CC_SANDY_DIRT;
+
+    // sandstone related
+    public static DungeonsBlock CC_SANDSTONE;
+    public static DungeonsBlock CC_BLUE_SANDSTONE;
+    public static DungeonsBlock CC_BROWN_SANDSTONE;
+    public static DungeonsBlock CC_DARK_BROWN_SANDSTONE;
+    public static DungeonsBlock CC_GRAY_SANDSTONE;
+    public static DungeonsBlock CC_GREEN_SANDSTONE;
+    public static DungeonsBlock CC_LIGHT_GREEN_SANDSTONE;
+    public static DungeonsBlock CC_ORANGE_SANDSTONE;
+    public static DungeonsBlock CC_PINK_SANDSTONE;
+    public static DungeonsBlock CC_PURPLE_SANDSTONE;
+    public static DungeonsBlock CC_RED_SANDSTONE;
+    public static DungeonsBlock CC_YELLOW_SANDSTONE;
     
     public static void init() {
+        // sand related
+        CC_SAND = new DungeonsFallingBlock(Material.SAND, BlockSoundGroup.SAND, JavaDungeons.CACTI_CANYON, "cc_sand");
+        CC_DENSE_SANDY_DIRT = new DungeonsBlock(Material.EARTH, BlockSoundGroup.GRAVEL, JavaDungeons.CACTI_CANYON, "cc_dense_sandy_dirt");
+        CC_SANDY_DIRT = new DungeonsBlock(Material.EARTH, BlockSoundGroup.GRAVEL, JavaDungeons.CACTI_CANYON, "cc_sandy_dirt");
         
+        // sandstone related
+        CC_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_sandstone");
+        CC_BLUE_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_blue_sandstone");
+        CC_BROWN_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_brown_sandstone");
+        CC_DARK_BROWN_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_dark_brown_sandstone");
+        CC_GRAY_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_gray_sandstone");
+        CC_GREEN_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_green_sandstone");
+        CC_LIGHT_GREEN_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_light_green_sandstone");
+        CC_ORANGE_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_orange_sandstone");
+        CC_PINK_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_pink_sandstone");
+        CC_PURPLE_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_purple_sandstone");
+        CC_RED_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_red_sandstone");
+        CC_YELLOW_SANDSTONE = new DungeonsBlock(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.CACTI_CANYON, "cc_yellow_sandstone");
     }
 
 }
