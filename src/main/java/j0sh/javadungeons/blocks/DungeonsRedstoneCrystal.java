16b9d52284d08d5804ff279588e0339303415f37
thejshua <joyesh708@gmail.com>
2020-05-29 20:28:27 -0500

    add redstone crystals

---
+++ src/main/java/j0sh/javadungeons/blocks/DungeonsRedstoneCrystal.java
@@ -0,0 +1,37 @@
+package j0sh.javadungeons.blocks;
+
+import j0sh.javadungeons.JavaDungeons;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.RedstoneBlock;
+import net.minecraft.entity.EntityContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.sound.BlockSoundGroup;
+
+public class DungeonsRedstoneCrystal extends RedstoneBlock {
+
+    // redstone crystal block
+
+    public BlockItem blockItem;
+
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 13.0D, 14.0D);
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
+        return SHAPE;
+    }
+
+    public DungeonsRedstoneCrystal(Material material, BlockSoundGroup sounds, ItemGroup group, String id) {
+        super(FabricBlockSettings.of(material).sounds(sounds).nonOpaque().lightLevel(12));
+        Registry.register(Registry.BLOCK, new Identifier(JavaDungeons.MOD_ID, id), this);
+        Registry.register(Registry.ITEM,new Identifier(JavaDungeons.MOD_ID, id), blockItem = new BlockItem(this, new Item.Settings().group(group)));
+    }
+}
--- src/main/java/j0sh/javadungeons/content/RedstoneMinesBlocks.java
+++ src/main/java/j0sh/javadungeons/content/RedstoneMinesBlocks.java
@@ -1,8 +1,15 @@
 package j0sh.javadungeons.content;
 
+import j0sh.javadungeons.JavaDungeons;
+import j0sh.javadungeons.blocks.*;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+
 public class RedstoneMinesBlocks {
+
+    public static DungeonsRedstoneCrystal RM_REDSTONE_CRYSTALS;
     
     public static void init() {
-
+        RM_REDSTONE_CRYSTALS = new DungeonsRedstoneCrystal(Material.STONE, BlockSoundGroup.STONE, JavaDungeons.REDSTONE_MINES, "rm_redstone_crystals");
     }
 }
