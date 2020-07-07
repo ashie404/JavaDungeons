1394e278584e959023943b72694c738a56f56c50
juniebyte <joyesh708@gmail.com>
2020-07-07 10:52:56 -0500

    remove broken paintings

--- src/main/java/juniebyte/javadungeons/content/Paintings.java
+++ src/main/java/juniebyte/javadungeons/content/Paintings.java
@@ -12,16 +12,10 @@ public class Paintings {
         registerPaintingMotive("arch_illager_portrait", 32, 32);
         registerPaintingMotive("crocodile", 32, 32);
         registerPaintingMotive("tropical", 32, 32);
-        registerPaintingMotive("aunt", 24, 24);
-        registerPaintingMotive("evoker", 24, 24);
-        registerPaintingMotive("mountain", 24, 24);
         registerPaintingMotive("redstone_mines", 32, 16);
-        registerPaintingMotive("foe_graffiti", 32, 16);
-        registerPaintingMotive("weak_graffiti", 32, 16);
         registerPaintingMotive("blue_fields", 16, 16);
         registerPaintingMotive("creeper", 16, 16);
         registerPaintingMotive("cacti_canyon", 32, 64);
-        registerPaintingMotive("obsidian_pinnacle", 34, 50);
     }
 
     private static void registerPaintingMotive(String name, int width, int height) {
