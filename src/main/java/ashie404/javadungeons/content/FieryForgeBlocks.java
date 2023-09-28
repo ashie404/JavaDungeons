8d4bd932353dc44e6f011a5092bb3cdd16aef8ec
June <22226164+juniebyte@users.noreply.github.com>
2023-09-27 19:50:00 -0500

    fiery forge content

--- src/main/java/ashie404/javadungeons/content/FieryForgeBlocks.java
+++ src/main/java/ashie404/javadungeons/content/FieryForgeBlocks.java
@@ -1,7 +1,12 @@
 package ashie404.javadungeons.content;
 
+import ashie404.javadungeons.JavaDungeons;
 import ashie404.javadungeons.blocks.*;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 
 public class FieryForgeBlocks {
 
@@ -29,5 +34,31 @@ public class FieryForgeBlocks {
         FF_DIRTY_DARK_STONE_TILES = new DungeonsSlabStairBlock(1.5F, 6.0F, BlockSoundGroup.STONE, "ff_dirty_dark_stone_tiles", "ff_dirty_dark_stone_tiles_slab", "ff_dirty_dark_stone_tiles_stairs");
         FF_DIRTY_DARK_STONE_TILES_1 = new DungeonsSlabStairBlock(1.5F, 6.0F, BlockSoundGroup.STONE, "ff_dirty_dark_stone_tiles_1", "ff_dirty_dark_stone_tiles_slab_1", "ff_dirty_dark_stone_tiles_stairs_1");
         FF_EMBLEMED_DARK_STONE = new DungeonsBlock(1.5F, 6.0F, BlockSoundGroup.STONE, "ff_emblemed_dark_stone");
+
+        // add to item group
+        ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(JavaDungeons.MOD_ID, "fiery_forge"))).register(content -> {
+            content.add(FF_DARK_STONE.base.blockItem);
+            content.add(FF_DARK_STONE.slab.blockItem);
+            content.add(FF_DARK_STONE.stairs.blockItem);
+            content.add(FF_DARK_COBBLESTONE.base.blockItem);
+            content.add(FF_DARK_COBBLESTONE.slab.blockItem);
+            content.add(FF_DARK_COBBLESTONE.stairs.blockItem);
+            content.add(FF_DARK_STONE_BRICKS.base.blockItem);
+            content.add(FF_DARK_STONE_BRICKS.slab.blockItem);
+            content.add(FF_DARK_STONE_BRICKS.stairs.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_BRICKS.blockItem);
+            content.add(FF_CRACKED_DARK_STONE_BRICKS.blockItem);
+            content.add(FF_CHISELED_DARK_STONE_BRICKS.blockItem);
+            content.add(FF_DARK_STONE_TILES.base.blockItem);
+            content.add(FF_DARK_STONE_TILES.slab.blockItem);
+            content.add(FF_DARK_STONE_TILES.stairs.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES.base.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES.slab.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES.stairs.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES_1.base.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES_1.slab.blockItem);
+            content.add(FF_DIRTY_DARK_STONE_TILES_1.stairs.blockItem);
+            content.add(FF_EMBLEMED_DARK_STONE.blockItem);
+        });
     }
 }
