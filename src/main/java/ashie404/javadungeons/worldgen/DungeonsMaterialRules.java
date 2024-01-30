a706c60088e97104ec1f625595fdeb8862b8e243
June <22226164+juniebyte@users.noreply.github.com>
2024-01-30 08:16:21 -0600

    material rules getting somewhere, add disks that have dungeon blocks whitelisted

--- src/main/java/ashie404/javadungeons/worldgen/DungeonsMaterialRules.java
+++ src/main/java/ashie404/javadungeons/worldgen/DungeonsMaterialRules.java
@@ -39,32 +39,97 @@ public class DungeonsMaterialRules {
     private static final MaterialRule SS_GRASS = makeStateRule(SoggySwampBlocks.SS_GRASS_BLOCK);
     private static final MaterialRule SS_DIRT = makeStateRule(SoggySwampBlocks.SS_DIRT);
 
+    // Construct final surface rule
     public static MaterialRule makeRules() {
         MaterialCondition atOrAboveWaterLvl = MaterialRules.water(-1, 0);
-        MaterialCondition stoneDepthFloor = MaterialRules.stoneDepth(0, false, VerticalSurfaceType.FLOOR);
-        MaterialCondition stoneDepthCeiling = MaterialRules.stoneDepth(0, false, VerticalSurfaceType.CEILING);
+        MaterialCondition soggySwamp = MaterialRules.biome(Biomes.SOGGY_SWAMP);
+        MaterialCondition creeperWoods = MaterialRules.biome(Biomes.CREEPER_WOODS);
+        MaterialCondition cactiCanyon = MaterialRules.biome(Biomes.CACTI_CANYON);
+        MaterialCondition cactiCanyonDesert = MaterialRules.biome(Biomes.CACTI_CANYON_DESERT);
 
         return MaterialRules.condition(MaterialRules.surface(), MaterialRules.sequence(
-            MaterialRules.condition(MaterialRules.biome(Biomes.SOGGY_SWAMP), MaterialRules.sequence(
-                MaterialRules.condition(stoneDepthCeiling, STONE),
-                MaterialRules.condition(atOrAboveWaterLvl, 
-                    MaterialRules.condition(stoneDepthFloor, SS_GRASS)
-                ), 
-                SS_DIRT 
-            )), 
-            MaterialRules.condition(MaterialRules.biome(Biomes.CREEPER_WOODS), MaterialRules.sequence(
-                MaterialRules.condition(stoneDepthCeiling, STONE),
-                MaterialRules.condition(atOrAboveWaterLvl, 
-                    MaterialRules.condition(stoneDepthFloor, CW_GRASS)
-                ), 
-                CW_DIRT
-            )), 
-            MaterialRules.condition(stoneDepthCeiling, STONE),
-            MaterialRules.condition(atOrAboveWaterLvl, 
-                MaterialRules.condition(stoneDepthFloor, GRASS)
-            ),
-            DIRT
+            // Above water surface rules
+            MaterialRules.condition(atOrAboveWaterLvl, MaterialRules.sequence(
+                SoggySwampSurfaceRule(soggySwamp, true),
+                CreeperWoodsSurfaceRule(creeperWoods, true),
+                CactiCanyonSurfaceRule(cactiCanyon, true),
+                CactiCanyonDesertSurfaceRule(cactiCanyonDesert, true),
+                DungeonsSurfaceRule(true)
+            )),
+            // Underwater
+            SoggySwampSurfaceRule(soggySwamp, false),
+            CreeperWoodsSurfaceRule(creeperWoods, false),
+            CactiCanyonSurfaceRule(cactiCanyon, false),
+            CactiCanyonDesertSurfaceRule(cactiCanyonDesert, false),
+            DungeonsSurfaceRule(false)
         ));
+    }
+
+    // Default surface rule for JavaDungeons biomes
+    private static MaterialRule DungeonsSurfaceRule(boolean aboveWater) {
+        return aboveWater ? 
+        // Above water surface builder
+            MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, GRASS),
+                MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), DIRT),
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, STONE),
+                STONE
+            ) :
+        // Underwater surface builder
+            MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), DIRT),
+                STONE
+            );
+    }
+
+    // Biome-specific surface rules
+    private static MaterialRule SoggySwampSurfaceRule(MaterialCondition biomeCondition, boolean aboveWater) {
+        return aboveWater ?
+        // Above water surface builder
+            MaterialRules.condition(biomeCondition, MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, SS_GRASS),
+                MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), SS_DIRT),
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, STONE),
+                STONE
+            )) :
+        // Underwater surface builder
+            MaterialRules.condition(biomeCondition, MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), SS_DIRT),
+                STONE
+            ));
+    }
+
+    private static MaterialRule CreeperWoodsSurfaceRule(MaterialCondition biomeCondition, boolean aboveWater) {
+        return aboveWater ?
+        // Above water surface builder
+            MaterialRules.condition(biomeCondition, MaterialRules.sequence(
+                    MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, CW_GRASS),
+                    MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), CW_DIRT),
+                    MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, STONE)
+            )) :
+        // Underwater surface builder
+            MaterialRules.condition(biomeCondition, MaterialRules.sequence(
+                MaterialRules.condition(MaterialRules.stoneDepth(1, true, VerticalSurfaceType.FLOOR), CW_DIRT),
+                STONE
+            ));
+    }
+
+    private static MaterialRule CactiCanyonSurfaceRule(MaterialCondition biomeCondition, boolean aboveWater) {
+        // TODO: Finish cacti canyon surface builder
+        return aboveWater ?
+        // Above water surface builder
+            MaterialRules.condition(biomeCondition, SANDSTONE) :
+        // Underwater surface builder
+            MaterialRules.condition(biomeCondition, STONE);
+    }
+
+    private static MaterialRule CactiCanyonDesertSurfaceRule(MaterialCondition biomeCondition, boolean aboveWater) {
+        // TODO: Finish cacti canyon desert surface builder
+        return aboveWater ?
+        // Above water surface builder
+            MaterialRules.condition(biomeCondition, SAND) :
+        // Underwater surface builder
+            MaterialRules.condition(biomeCondition, STONE);
     }
 
     private static MaterialRule makeStateRule(Block block) {
