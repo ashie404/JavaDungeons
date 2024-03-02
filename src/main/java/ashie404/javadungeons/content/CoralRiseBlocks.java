9530f7399a400caf9826f700c119d0583084c7d8
June <22226164+juniebyte@users.noreply.github.com>
2024-03-02 06:50:27 -0600

    coral fan textures separated, add short bubble weed

--- src/main/java/ashie404/javadungeons/content/CoralRiseBlocks.java
+++ src/main/java/ashie404/javadungeons/content/CoralRiseBlocks.java
@@ -180,18 +180,28 @@ public class CoralRiseBlocks {
         RegistryHelper.registerBlock(new UnderwaterTallPlant(FabricBlockSettings.copyOf(Material.OFFSET_XZ_PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_tall_seaweed");
     public static final Block CR_BUBBLE_WEED =
         RegistryHelper.registerBlock(new UnderwaterTallPlant(FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_bubble_weed");
+    public static final Block CR_SHORT_BUBBLE_WEED =
+        RegistryHelper.registerBlock(new UnderwaterPlant(0, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_short_bubble_weed");
     public static final Block CR_FIRE_CORAL =
         RegistryHelper.registerBlock(new UnderwaterPlant(0, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_fire_coral");
     public static final Block CR_TUBE_CORAL =
         RegistryHelper.registerBlock(new UnderwaterPlant(0, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_tube_coral");
     public static final Block CR_TALL_TUBE_CORAL =
         RegistryHelper.registerBlock(new UnderwaterTallPlant(FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_tall_tube_coral");
+    public static final Block CR_TEAL_CORAL_FAN =
+        RegistryHelper.registerBlock(new UnderwaterPlant(1, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_teal_coral_fan");
+    public static final Block CR_PURPLE_CORAL_FAN =
+        RegistryHelper.registerBlock(new UnderwaterPlant(1, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_purple_coral_fan");
     public static final Block CR_BLUE_CORAL_FAN =
         RegistryHelper.registerBlock(new UnderwaterPlant(1, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_blue_coral_fan");
-    public static final Block CR_YELLOW_CORAL_FAN =
-        RegistryHelper.registerBlock(new UnderwaterPlant(1, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_yellow_coral_fan");
-    public static final Block CR_LARGE_CORAL_COLONY =
-        RegistryHelper.registerBlock(new UnderwaterPlant(2, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_large_coral_colony");
+    public static final Block CR_RED_LARGE_CORAL_FAN =
+        RegistryHelper.registerBlock(new UnderwaterPlant(2, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_red_large_coral_fan");
+    public static final Block CR_PURPLE_LARGE_CORAL_FAN =
+        RegistryHelper.registerBlock(new UnderwaterPlant(2, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_purple_large_coral_fan");
+    public static final Block CR_GREEN_LARGE_CORAL_COLONY =
+        RegistryHelper.registerBlock(new UnderwaterPlant(2, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_green_large_coral_colony");
+    public static final Block CR_PURPLE_LARGE_CORAL_COLONY =
+        RegistryHelper.registerBlock(new UnderwaterPlant(2, FabricBlockSettings.copyOf(Material.PLANT).sounds(BlockSoundGroup.WET_GRASS)), "cr_purple_large_coral_colony");
     public static final Block CR_RED_LUMINOUS_MUSHROOMS =
         RegistryHelper.registerBlock(new UnderwaterPlant(0, Material.PLANT), "cr_red_luminous_mushrooms");
     public static final Block CR_ORANGE_LUMINOUS_MUSHROOMS =
@@ -307,6 +317,7 @@ public class CoralRiseBlocks {
             content.add(CR_SEAWEED);
             content.add(CR_TALL_SEAWEED);
             content.add(CR_BUBBLE_WEED);
+            content.add(CR_SHORT_BUBBLE_WEED);
             content.add(CR_FIRE_CORAL);
             content.add(CR_TUBE_CORAL);
             content.add(CR_TALL_TUBE_CORAL);
